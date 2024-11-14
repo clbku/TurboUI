bc7467b5c40c71bb6ef6abe0b041afbc16fb9080
Mklop0098 <hoangsonfds@gmail.com>
2024-11-14 18:46:22 +0700

    feat(AppLayout, DataTable): remove auto redirect, resize table height

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,109 +1,98 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import { Button, Divider, Flex, Layout, Menu } from 'antd';
+import { Outlet, useNavigate } from "react-router-dom";
+import { Button, Divider, Flex, Layout, Menu } from "antd";
 
-import { FAIcon } from '@repo/vicon';
-import { useEffect, useState } from 'react';
+import { FAIcon } from "@repo/vicon";
+import { useEffect, useState } from "react";
 
-import './AppLayout.css';
-import { AppLayoutRoute } from './types';
-import { SubFeatures } from './SubFeatures';
+import "./AppLayout.css";
+import { AppLayoutRoute } from "./types";
+import { SubFeatures } from "./SubFeatures";
 
 type AppLayoutProps = {
-    logo?: any
-    routes: AppLayoutRoute[]
-    defaultRoute?: string
-    collapsed?: boolean
-}
+  logo?: any;
+  routes: AppLayoutRoute[];
+  defaultRoute?: string;
+  collapsed?: boolean;
+};
 
-export const AppLayout: React.FC<AppLayoutProps> = (props) =>
-{
-    const { logo, routes, defaultRoute } = props;
+export const AppLayout: React.FC<AppLayoutProps> = (props) => {
+  const { logo, routes, defaultRoute } = props;
 
-    const [collapsed, setCollapsed] = useState(props.collapsed || false);
-    const [selectedKeys, setSelectedKeys] = useState<string[]>(defaultRoute ? [defaultRoute] : []);
+  const [collapsed, setCollapsed] = useState(props.collapsed || false);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(
+    defaultRoute ? [defaultRoute] : []
+  );
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const navigateTo = (path: string) =>
-    {
-        setSelectedKeys([path]);
-        navigate(path);
-    };
+  const navigateTo = (path: string) => {
+    setSelectedKeys([path]);
+    navigate(path);
+  };
 
-    useEffect(() =>
-    {
-        navigate(defaultRoute || '');
-    }, []);
+  // useEffect(() =>
+  // {
+  //     navigate(defaultRoute || '');
+  // }, []);
 
-    return (
-        <>
-            <Layout
-                style={{ width: '100vw', height: '100vh' }}
-                hasSider
-            >
-                <Flex
-                    style={{ width: collapsed ? 64 : 256, justifyContent: 'flex-end' }}
-                    className='sidebar'
-                    vertical
-                >
-                    {logo && (
-                        <>
-                            <Flex className="app-brand">
-                                <img
-                                    src={logo}
-                                    style={{ width: 'auto', height: '100%', maxHeight: collapsed ? 48 : 64 }}
-                                    onClick={() => navigateTo(defaultRoute || '')}
-                                />
-                            </Flex>
-                            <Divider />
-                        </>
-                    )}
-                    <Flex className='app-feature'>
-                        <Menu
-                            mode={'inline'}
-                            selectedKeys={selectedKeys}
-                            items={routes?.map((route) =>
-                                ({
-                                    key: route.path,
-                                    label: route.label,
-                                    title: route.label,
-                                    icon: (
-                                        <FAIcon
-                                            icon={route.icon}
-                                            size="1rem"
-                                        />
-                                    ),
-                                    onClick: () => navigateTo(route.path),
-                                }),
-                            )}
-                        />
-                    </Flex>
-                    <Flex
-                        className='app-sub-feature'
-                    >
-                        <SubFeatures />
-                    </Flex>
-                    <Divider style={{ margin: '2px' }} />
-                    <Flex className='app-collapse-button'>
-                        <Button onClick={() => setCollapsed(!collapsed)}>
-                            <FAIcon
-                                icon={collapsed ? 'chevron-double-right' : 'chevron-double-left'}
-                                size='1rem'
-                            />
-                        </Button>
-                    </Flex>
-                </Flex>
-                <Divider
-                    style={{ margin: '0 2px', height: '100vh' }}
-                    type='vertical'
+  return (
+    <>
+      <Layout style={{ width: "100vw", height: "100vh" }} hasSider>
+        <Flex
+          style={{ width: collapsed ? 64 : 256, justifyContent: "flex-end" }}
+          className="sidebar"
+          vertical
+        >
+          {logo && (
+            <>
+              <Flex className="app-brand">
+                <img
+                  src={logo}
+                  style={{
+                    width: "auto",
+                    height: "100%",
+                    maxHeight: collapsed ? 48 : 64,
+                  }}
+                  onClick={() => navigateTo(defaultRoute || "")}
                 />
-                <Layout
-                    className="app-content"
-                >
-                    <br />
-                    <Outlet />
-                </Layout>
-            </Layout>
-        </>
-    );
+              </Flex>
+              <Divider />
+            </>
+          )}
+          <Flex className="app-feature">
+            <Menu
+              mode={"inline"}
+              selectedKeys={selectedKeys}
+              items={routes?.map((route) => ({
+                key: route.path,
+                label: route.label,
+                title: route.label,
+                icon: <FAIcon icon={route.icon} size="1rem" />,
+                onClick: () => navigateTo(route.path),
+              }))}
+            />
+          </Flex>
+          <Flex className="app-sub-feature">
+            <SubFeatures />
+          </Flex>
+          <Divider style={{ margin: "2px" }} />
+          <Flex className="app-collapse-button">
+            <Button onClick={() => setCollapsed(!collapsed)}>
+              <FAIcon
+                icon={
+                  collapsed ? "chevron-double-right" : "chevron-double-left"
+                }
+                size="1rem"
+              />
+            </Button>
+          </Flex>
+        </Flex>
+        <Divider style={{ margin: "0 2px", height: "100vh" }} type="vertical" />
+        <Layout className="app-content">
+          <br />
+          <Outlet />
+        </Layout>
+      </Layout>
+    </>
+  );
 };

62053bd0ef79a51edd14d9aa134121097eeaf80a
Mklop0098 <hoangsonfds@gmail.com>
2024-11-17 09:51:56 +0700

    feat(Datatable): refactor

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,9 +1,8 @@
-import { FAIcon } from "@repo/vicon";
-import { Button, Flex, Input, Table } from "antd";
-import { ColumnsType } from "antd/es/table";
-import { useEffect, useRef, useState } from "react";
-import { ButtonType } from "./type";
-import { TableRowSelection } from "antd/es/table/interface";
+import { FAIcon } from '@repo/vicon';
+import { Button, Flex, Input, Table } from 'antd';
+import { useEffect, useRef, useState } from 'react';
+import { ButtonType } from './type';
+import { TableRowSelection } from 'antd/es/table/interface';
 
 type DataTableProps<T> = {
   dataSource: T[];
@@ -12,78 +11,73 @@ type DataTableProps<T> = {
   onfilter?: (keyword: string) => void;
 } & { [key: string]: any };
 
-export function DataTable<T>(props: DataTableProps<T>): JSX.Element {
-  const { dataSource, buttons, rowSelection, maxHeight, ...tableProps } = props;
-  const { onfilter } = props;
+export function DataTable<T>(props: DataTableProps<T>): JSX.Element
+{
+    const { dataSource, buttons, rowSelection, ...tableProps } = props;
+    const { onfilter } = props;
 
-  const [searchWord, setSearchWord] = useState<string>("");
-  const tableRef = useRef<any>(null);
+    const [searchWord, setSearchWord] = useState<string>('');
 
-  const [tableHeight, setTableHeight] = useState(window.innerHeight);
 
-  useEffect(() => {
-    onfilter && onfilter(searchWord);
-  }, [searchWord]);
+    useEffect(() =>
+    {
+        onfilter && onfilter(searchWord);
+    }, [searchWord]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setTableHeight(window.innerHeight - 155);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        flex: 1,
-      }}
-    >
-      <Flex justify="space-between" align="center">
-        <Flex>
-          {buttons &&
-            buttons.map((buttons, key) => {
-              const { icon, label, onclick, ...buttonProps } = buttons;
-              return (
-                <Button
-                  key={key}
-                  style={{
-                    display: "flex",
-                    width: "fit-content",
-                    margin: "12px 12px 12px 0",
-                  }}
-                  {...buttonProps}
-                  onClick={onclick}
-                >
-                  <FAIcon icon={icon} size="14px" />
-                  <div>{label}</div>
-                </Button>
-              );
-            })}
-        </Flex>
-        {onfilter && (
-          <Input
-            placeholder="Search for word"
+    return (
+        <div
             style={{
-              maxWidth: 300,
-              marginRight: "4px",
+                display: 'flex',
+                flexDirection: 'column',
+                flex: 1,
             }}
-            onChange={(e) => setSearchWord(e.target.value)}
-          />
-        )}
-      </Flex>
-      <Table<T>
-        dataSource={dataSource}
-        rowSelection={{ type: "checkbox", ...rowSelection }}
-        {...tableProps}
-        scroll={{ y: tableHeight }}
-        style={{ flex: 1 }}
-      />
-    </div>
-  );
+        >
+            <Flex
+                justify="space-between"
+                align="center"
+            >
+                <Flex>
+                    {buttons &&
+            buttons.map((buttons, key) =>
+            {
+                const { icon, label, onclick, ...buttonProps } = buttons;
+                return (
+                    <Button
+                        key={key}
+                        style={{
+                            display: 'flex',
+                            width: 'fit-content',
+                            margin: '12px 12px 12px 0',
+                        }}
+                        {...buttonProps}
+                        onClick={onclick}
+                    >
+                        <FAIcon
+                            icon={icon}
+                            size="14px"
+                        />
+                        <div>{label}</div>
+                    </Button>
+                );
+            })}
+                </Flex>
+                {onfilter && (
+                    <Input
+                        placeholder="Search for word"
+                        style={{
+                            maxWidth: 300,
+                            marginRight: '4px',
+                        }}
+                        onChange={(e) => setSearchWord(e.target.value)}
+                    />
+                )}
+            </Flex>
+            <Table<T>
+                dataSource={dataSource}
+                rowSelection={{ type: 'checkbox', ...rowSelection }}
+                {...tableProps}
+                style={{ flex: 1 }}
+            />
+        </div>
+    );
 }
